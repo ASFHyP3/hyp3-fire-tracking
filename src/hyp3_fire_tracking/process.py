3a010f689b32923ba2f4ad4fa58867adb63cb8a9
mfangaritav <mf.angarita52@gmail.com>
2026-07-17 10:00:41 -0800

    filter filename

--- src/hyp3_fire_tracking/process.py
+++ src/hyp3_fire_tracking/process.py
@@ -47,19 +47,20 @@ def download_data(input_bucket: str, input_prefix: str, output: str = 'output') 
     buck = s3.Bucket(input_bucket)
     for s3_object in tqdm(buck.objects.filter(Prefix=f'{input_prefix}')):
         path, filename = os.path.split(s3_object.key)
-        suffix = filename.split('_')[1]
-        sat = get_sat(suffix)
-        start = dt.datetime.strptime('_'.join([filename.split('_')[2], filename.split('_')[3]]), 'd%Y%m%d_t%H%M%S%f')
-        end = dt.datetime.strptime('_'.join([filename.split('_')[2], filename.split('_')[4]]), 'd%Y%m%d_e%H%M%S%f')
-        sstart = start.strftime('%Y%m%d%H%M%S%f')
-        send = end.strftime('%Y%m%d%H%M%S%f')
-        corr = filename.split('_')[6][0:16]
-        newfilename = f'EFIRE_VIIRSI_v1r3_{sat}_s{sstart}_e{send}_c{corr}.nc'
-        folder = Path(start.strftime(f'{output}/staging/incoming/pass_{sat}_%Y%m%dT%H%M'))
-        folder.mkdir(parents=True, exist_ok=True)
-        buck.download_file(s3_object.key, f'{str(folder)}/{newfilename}')
-        ready = Path(f'{str(folder)}/READY.flag')
-        ready.touch()
+        if '_' in filename:
+            suffix = filename.split('_')[1]
+            sat = get_sat(suffix)
+            start = dt.datetime.strptime('_'.join([filename.split('_')[2], filename.split('_')[3]]), 'd%Y%m%d_t%H%M%S%f')
+            end = dt.datetime.strptime('_'.join([filename.split('_')[2], filename.split('_')[4]]), 'd%Y%m%d_e%H%M%S%f')
+            sstart = start.strftime('%Y%m%d%H%M%S%f')
+            send = end.strftime('%Y%m%d%H%M%S%f')
+            corr = filename.split('_')[6][0:16]
+            newfilename = f'EFIRE_VIIRSI_v1r3_{sat}_s{sstart}_e{send}_c{corr}.nc'
+            folder = Path(start.strftime(f'{output}/staging/incoming/pass_{sat}_%Y%m%dT%H%M'))
+            folder.mkdir(parents=True, exist_ok=True)
+            buck.download_file(s3_object.key, f'{str(folder)}/{newfilename}')
+            ready = Path(f'{str(folder)}/READY.flag')
+            ready.touch()
 
 
 def process_fire_tracking(
